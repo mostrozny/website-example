e50404614dbcd1bbfd29cd38222d71d61fb79281
mostrozny <ostrozny.michal@gmail.com>
2018-09-13 14:02:19 +0200

    gallery, hamburger

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("Helloo!");
 
+    $(document).ready(function () {
+        var mySwiper = new Swiper ('.swiper-container', {
+            // Optional parameters
+            direction: 'horizontal',
+            loop: true,
+            autoplay: {
+                delay: 5000,
+            },
+            flipEffect: {
+                rotate: 30,
+                slideShadows: false,
+            },
+        })
+    });
 });
